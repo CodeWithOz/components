415fd9fbf039564c491f599dbffc89f7d19aa088
Uche Ozoemena <ucheozoemena@gmail.com>
2018-11-25 05:57:25 +0100

    Assign comment and time dynamically using props

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail
+        author="Jane" timeAgo="Today at 4:45PM"
+        commentText="Awesome!"
+      />
+      <CommentDetail
+        author="Alex" timeAgo="Today at 2:00PM"
+        commentText="Nice blog post!"
+      />
+      <CommentDetail
+        author="Sam" timeAgo="Yesterday at 5:00PM"
+        commentText="I love this! 😄"
+      />
     </div>
   );
 };
