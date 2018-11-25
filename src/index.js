93d3662ec912131493f577a2bc5bada860b9f651
Uche Ozoemena <ucheozoemena@gmail.com>
2018-11-25 06:44:07 +0100

    Nest components using props.children

--- src/index.js
+++ src/index.js
@@ -2,22 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
+      <ApprovalCard>
+        <CommentDetail
         author="Jane" timeAgo="Today at 4:45PM"
         commentText="Awesome!" avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex" timeAgo="Today at 2:00PM"
-        commentText="Nice blog post!" avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Sam" timeAgo="Yesterday at 5:00PM"
-        commentText="I love this! 😄" avatar={faker.image.avatar()}
-      />
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex" timeAgo="Today at 2:00PM"
+          commentText="Nice blog post!" avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam" timeAgo="Yesterday at 5:00PM"
+          commentText="I love this! 😄" avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
