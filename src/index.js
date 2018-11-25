933a4f5ee41fac545264757c9515153c19144fb2
Uche Ozoemena <ucheozoemena@gmail.com>
2018-11-25 06:08:40 +0100

    Pass in avatar as a prop

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
       <CommentDetail
         author="Jane" timeAgo="Today at 4:45PM"
-        commentText="Awesome!"
+        commentText="Awesome!" avatar={faker.image.avatar()}
       />
       <CommentDetail
         author="Alex" timeAgo="Today at 2:00PM"
-        commentText="Nice blog post!"
+        commentText="Nice blog post!" avatar={faker.image.avatar()}
       />
       <CommentDetail
         author="Sam" timeAgo="Yesterday at 5:00PM"
-        commentText="I love this! 😄"
+        commentText="I love this! 😄" avatar={faker.image.avatar()}
       />
     </div>
   );
